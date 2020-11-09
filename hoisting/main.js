b49d80dace53b5c125cfbbdd8dcb0f3ca0a99142
Ruth Newman <ruthnewman@gmail.com>
2020-11-09 00:14:06 +0000

    playground to explore hoisting and partial hoisting

---
+++ hoisting/main.js
@@ -0,0 +1,13 @@
+console.log('1------');
+
+// undefined
+console.log(key);
+
+// hoisted
+console.log(cantar());
+
+var key = 'dawn';
+
+function cantar() {
+    console.log('aay no hay que llorar...');
+}
