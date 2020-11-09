cd2cd88b2a361e62f1ab059bd06965254e062a91
Ruth Newman <ruthnewman@gmail.com>
2020-11-09 16:51:12 +0000

    hoisting with different contexts

---
+++ hoisting/faveFood.js
@@ -0,0 +1,11 @@
+var favouriteFood = "sushi";
+
+var foodThoughts = function() {
+    console.log("Original favourite food: " + favouriteFood);
+
+    var favouriteFood = "ice cream";
+
+    console.log("New favourite food: " + favouriteFood);
+}
+
+foodThoughts();
