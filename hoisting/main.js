abfa6500f087ac92e7cd60ce1038220a16e05e2c
Ruth Newman <ruthnewman@gmail.com>
2020-11-09 16:48:32 +0000

    playground of variable and function hoisting when declared twice

--- hoisting/main.js
+++ hoisting/main.js
@@ -8,6 +8,13 @@ console.log(cantar());
 
 var key = 'dawn';
 
+// function expression
+var letra = function() {
+    console.log('la vida es un carnaval, hay que vivir cantando...');
+}
+
+console.log(letra());
+
 function cantar() {
     console.log('aay no hay que llorar...');
 }
