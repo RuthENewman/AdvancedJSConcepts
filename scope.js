9ae52592b200c0456870a58b8bda99c1b2bbec71
Ruth Newman <ruthnewman@gmail.com>
2020-11-11 23:59:34 +0000

    for loops with block scope using let

--- scope.js
+++ scope.js
@@ -9,11 +9,11 @@ if (5 > 4) {
 // will error due to block scoping with const & let
 // console.log(secret);
 
-
 function loop() {
-    for(var i = 0; i < 5; i++) {
+    for(let i = 0; i < 5; i++) {
         console.log(i);
     }
+    // will error due to block scoping with const & let
     console.log('final', i);
 }
 
