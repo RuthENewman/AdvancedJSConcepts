6960e43bcdf69825ab06778f34c90fc6134a168a
Ruth Newman <ruthnewman@gmail.com>
2020-11-15 23:48:03 +0000

    dynamic and lexical scope

---
+++ scope/dynamic-v-lexical.js
@@ -0,0 +1,28 @@
+const a = function() {
+    console.log('a', this)
+    const b = function() {
+        console.log('b', this)
+        const c = {
+            hi: function() {
+                console.log('c', this)
+            }
+        }
+        c.hi();
+    }
+    b();
+}
+
+a();
+
+// 
+
+const obj = {
+    name: 'Charlotte',
+    sing() {
+        console.log('a', this);
+        var anotherFunc = function() {
+            console.log('b', this);
+        }
+        anotherFunc();
+    }
+}
