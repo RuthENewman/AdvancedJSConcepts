e426fb28b944e73467e6d2b66009d9ffe95a45e0
Ruth Newman <ruthnewman@gmail.com>
2020-11-19 23:34:10 +0000

    types playground

---
+++ types/js-types.js
@@ -0,0 +1,13 @@
+let five = typeof 5;
+let yes = typeof true;
+let quote = typeof 'Bored now';
+let mystery = typeof undefined;
+let nothing = typeof null;
+// Symbol('just me')
+// {}
+
+console.log(five);
+console.log(yes);
+console.log(quote);
+console.log(mystery);
+console.log(nothing);
