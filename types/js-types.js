bf98d87e8a0b389ce02dd33fdd04e7f42a95e67f
Ruth Newman <ruthnewman@gmail.com>
2020-11-20 00:04:01 +0000

    more types

--- types/js-types.js
+++ types/js-types.js
@@ -3,11 +3,26 @@ let yes = typeof true;
 let quote = typeof 'Bored now';
 let mystery = typeof undefined;
 let nothing = typeof null;
-// Symbol('just me')
-// {}
+let symbolic = typeof Symbol('just me');
+let object = typeof {};
+let array = typeof [];
+let funktion = typeof function() {};
 
+// number
 console.log(five);
+// boolean
 console.log(yes);
+// string
 console.log(quote);
+// undefined
 console.log(mystery);
+// object
 console.log(nothing);
+// symbol
+console.log(symbolic);
+// object
+console.log(object);
+// object
+console.log(array);
+// function
+console.log(funktion);
