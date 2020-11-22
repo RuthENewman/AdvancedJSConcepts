ade72c00a836bf950839a825378ecf6a9fefda49
Ruth Newman <ruthnewman@gmail.com>
2020-11-22 00:07:35 +0000

    Array.isArray()

--- types/type-conversion.js
+++ types/type-conversion.js
@@ -1,3 +1,24 @@
 let yes = true.toString();
 
 console.log(typeof yes, yes);
+
+let yep = Boolean(true).toString();
+
+console.log(typeof yep, yep);
+
+
+//
+
+var array = [1,2,3];
+
+var arrayTwo = {
+    0: 1,
+    1: 2,
+    2: 2
+};
+
+console.log(typeof array);
+console.log(typeof arrayTwo);
+
+console.log(Array.isArray([1,2,3,4,5]));
+console.log(Array.isArray({}));
