0bd8998e54d44ef45547285f60e55e27a0d04fd5
Ruth Newman <ruthnewman@gmail.com>
2020-11-27 23:59:12 +0000

    type conversion

--- types/type-coercion.js
+++ types/type-coercion.js
@@ -1,1 +1,29 @@
-1 == "1"
+1 == "1" // 1 == 1
+
+var jan = (false == "") 
+console.log('jan', jan); // true
+
+var feb = (false == []); 
+console.log('feb', feb);
+
+var march = (false == {}); //
+console.log('march', march);
+
+var april = ("" == 0);
+console.log('april', april);     
+var may = ("" == []);
+console.log('may', may);     
+
+var june = ("" == {}) //
+console.log('june', june);
+
+var july = (0 == []) // false
+console.log('july', july);
+
+var aug = (0 == {}) 
+// false 
+console.log('aug', aug); 
+
+var sept = (0 == null)  
+// false
+console.log('sept', sept);
