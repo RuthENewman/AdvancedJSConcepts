6533c82fb75e8e9c5758903eb6c5d435f3c1523d
Ruth Newman <ruthnewman@gmail.com>
2020-12-07 23:43:08 +0000

    greater abstraction

--- closures/hof.js
+++ closures/hof.js
@@ -1,23 +1,28 @@
 const giveAccessTo = (username) => `Access granted to ${username}`;
 
-function authenticate(verify) {
+function authenticate(verify, person) {
     let array = [];
     for (let i = 0; i < verify; i++) {
         array.push(i);
     }
-    return true;
+    return giveAccessTo(person.name);
 } 
+
+function sing(verify, person) {
+    return `${person.name} goes la la la la la`;
+}
 
 function letPersonLogin(person, fn) {
     if (person.level === 'admin') {
-        fn(5000000);
+        return fn(1000000, person);
     } else if (person.level === 'user') {
-        fn(1000000);
+        return fn(5000000, person);
     }
-    return giveAccessTo(person.name);
 }
 
 let captain = letPersonLogin({level: 'admin', name: 'Raymond'}, authenticate);
 let detective = letPersonLogin({level: 'user', name: 'Jake'}, authenticate);
+let fito = letPersonLogin({level: 'user', name: 'Fito'}, sing);
 
 console.log(captain, detective);
+console.log(fito);
