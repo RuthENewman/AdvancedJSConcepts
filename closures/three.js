4547c97cbbede5dd082e80b621cefc4838d3dd0b
Ruth Newman <ruthnewman@gmail.com>
2020-12-11 00:08:48 +0000

    exercise with solution

---
+++ closures/three.js
@@ -0,0 +1,7 @@
+const array = [1,2,3,4];
+for(var i=0; i < array.length; i++) {
+    // setTimeout(function() {
+    //     console.log(`I am at index ${i}`);
+    // }, 3000);
+    console.log(`I am at index ${i}`);
+}
