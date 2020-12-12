0571e54491e2bef7f1a3bb3eb99c875acb20b9f8
Ruth Newman <ruthnewman@gmail.com>
2020-12-12 23:53:49 +0000

    prototypal inheritance

--- closures/three.js
+++ closures/three.js
@@ -1,7 +1,38 @@
+// SOLUTION ONE
+
+// const array = [1,2,3,4];
+// for(var i=0; i < array.length; i++) {
+//     // setTimeout(function() {
+//     //     console.log(`I am at index ${i}`);
+//     // }, 3000);
+//     console.log(`I am at index ${i}`);
+// }
+
+// SOLUTION TWO
+
+// const array = [1,2,3,4];
+// for(let i=0; i < array.length; i++) {
+//     setTimeout(function() {
+//         console.log(`I am at index ${i}`);
+//     }, 3000);
+// }
+
+// SOLUTION THREE WITH 1,2,3,4
+
+// const array = [1,2,3,4];
+// for(let i=0; i < array.length; i++) {
+//     setTimeout(function() {
+//         console.log(`I am at index ${array[i]}`);
+//     }, 3000);
+// }
+
+// SOLUTION FOUR
+
 const array = [1,2,3,4];
-for(var i=0; i < array.length; i++) {
-    // setTimeout(function() {
-    //     console.log(`I am at index ${i}`);
-    // }, 3000);
-    console.log(`I am at index ${i}`);
+for(let i=0; i < array.length; i++) {
+    (function(closureI) {
+        setTimeout(function() {
+        console.log(`I am at index ${array[closureI]}`);
+    }, 3000);
+    })(i);
 }
---
+++ prototypal-inheritance/proto-one.js
@@ -0,0 +1,21 @@
+const array = [];
+
+array.__proto__;
+
+array.__proto__.__proto__;
+
+///
+
+function a() {
+
+}
+
+a.__proto__;
+
+a.__proto__.__proto__;
+
+// 
+
+const obj1 = {};
+
+obj1.__proto__;
