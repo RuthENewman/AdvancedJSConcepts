2a5bc57e588273022d85cf63d5da3ac01f7aa779
Ruth Newman <ruthnewman@gmail.com>
2020-12-19 20:51:05 +0000

    added exercises

---
+++ prototypal-inheritance/map.js
@@ -0,0 +1,4 @@
+//#Bonus
+// Mofify .map() to print '🗺' at the end of each item.
+console.log([1,2,3].map())
+//1🗺, 2🗺, 3🗺
---
+++ prototypal-inheritance/time.js
@@ -0,0 +1,7 @@
+// Extending the functionality of a built in object
+
+// #1 The Date object
+// New mothod .previousYear();
+
+new Date('2020-12-19').previousYear();
+// returns 2019
