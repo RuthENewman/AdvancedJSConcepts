b8addebc94f9e4bbf5a0289f1616dc4feaac1378
Ruth Newman <ruthnewman@gmail.com>
2020-12-20 23:50:17 +0000

    date solution

--- prototypal-inheritance/time.js
+++ prototypal-inheritance/time.js
@@ -3,5 +3,11 @@
 // #1 The Date object
 // New mothod .previousYear();
 
-new Date('2020-12-19').previousYear();
+Date.prototype.previousYear = function() {
+    return this.getFullYear() - 1;
+}
+
+const lastYear = new Date('2020-12-20').previousYear();
+
+console.log(lastYear);
 // returns 2019
