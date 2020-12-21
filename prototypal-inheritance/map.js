3ac9057b884a3e30060a08aae3ee3b0773d3ab20
Ruth Newman <ruthnewman@gmail.com>
2020-12-21 23:54:47 +0000

    array map function modification exercise

--- prototypal-inheritance/map.js
+++ prototypal-inheritance/map.js
@@ -1,4 +1,12 @@
 //#Bonus
 // Mofify .map() to print '🗺' at the end of each item.
+Array.prototype.map = function() {
+    let array = [];
+    for (let i = 0; i < this.length; i++) {
+        array.push((this[i] + '🗺)'));
+    }
+    return array;
+}
+
 console.log([1,2,3].map())
 //1🗺, 2🗺, 3🗺
