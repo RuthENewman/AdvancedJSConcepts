fc9567563ec41c11926403574703baf666e7f32b
Ruth Newman <ruthnewman@gmail.com>
2020-12-23 23:54:05 +0000

    creating objects

---
+++ object-oriented-programming/factory-functions.js
@@ -0,0 +1,17 @@
+const elf = {
+    name: 'Orwell',
+    weapon: 'bow',
+    attack() {
+        return `attack with ${elf.weapon}`;
+    }
+}
+
+elf.attack();
+
+const elf2 = {
+    name: 'Sally',
+    weapon: 'bow',
+    attack() {
+        return `attack with ${elf.weapon}`;
+    }
+}
