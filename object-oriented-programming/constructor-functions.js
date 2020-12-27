773fd716004a3fdee748f3fed7ec6a9e211331d4
Ruth Newman <ruthnewman@gmail.com>
2020-12-27 23:47:05 +0000

    constructor function

---
+++ object-oriented-programming/constructor-functions.js
@@ -0,0 +1,11 @@
+function elf(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+}
+
+
+const peter = new elf('Peter', 'stones');
+// console.log(peter.attack());
+
+const sam = new elf('Peter', 'fire');
+// console.log(sam.attack());
