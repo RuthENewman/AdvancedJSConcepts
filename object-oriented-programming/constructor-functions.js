6457b561fdc0c61eaf237b952265622aac9c3962
Ruth Newman <ruthnewman@gmail.com>
2020-12-30 23:55:32 +0000

    playground with build function

--- object-oriented-programming/constructor-functions.js
+++ object-oriented-programming/constructor-functions.js
@@ -7,8 +7,17 @@ Elf.prototype.attack = function() {
     return `attack with ${this.weapon}`;
 }
 
+Elf.prototype.build = function() {
+    const self = this;
+    function building() {
+        return `${self.name} builds a house`;
+    }
+    return building();
+}
+
 const peter = new Elf('Peter', 'stones');
 console.log(Elf.prototype === peter.__proto__); // true
+console.log(peter.build());
 
 const sam = new Elf('Peter', 'fire');
 console.log(sam.attack());
