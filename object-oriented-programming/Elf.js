67e0a94ed83cfe0f9c0cc9539486417395a67fb4
Ruth Newman <ruthnewman@gmail.com>
2020-12-31 00:02:46 +0000

    refactored with ES6 class

---
+++ object-oriented-programming/Elf.js
@@ -0,0 +1,17 @@
+// ES6 class
+class Elf {
+    constructor(name, weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+    attack() {
+        return `attack with ${this.weapon}`;
+    }
+}
+
+const peter = new Elf('Peter', 'stones');
+console.log(peter.attack());
+
+const sam = new Elf('Peter', 'fire');
+console.log(sam.attack());
+
