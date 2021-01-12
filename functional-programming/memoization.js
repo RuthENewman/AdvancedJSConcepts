de7a687208126310cfd750b0a82dcb7a29f0f263
Ruth Newman <ruthnewman@gmail.com>
2021-01-12 23:30:04 +0000

    refactored memoization example

--- functional-programming/memoization.js
+++ functional-programming/memoization.js
@@ -7,18 +7,22 @@ addTo80(5); // calculates and returns 85
 addTo80(5); // calculates and returns 85
 addTo80(5); // calculates and returns 85
 
-let cache = {};
-function memoizeAddTo80(num) {
-    if (num in cache) {
-        return cache[num];
-    } else {
-        console.log('slowly......');
-        cache[num] = num + 80;
-        return cache[num];
+function memoizeAddTo80() {
+    let cache = {};
+    return function(num) {
+        if (num in cache) {
+            return cache[num];
+        } else {
+            console.log('slowly......');
+            cache[num] = num + 80;
+            return cache[num];
+        }
     }
 }
 
-console.log('1', memoizeAddTo80(5)); // calculates and returns 85
-console.log('2', memoizeAddTo80(5)); // returns 85 from the cache object
-console.log('2', memoizeAddTo80(5)); // returns 85 from the cache object
+const memoized = memoizeAddTo80();
+
+console.log('1', memoized(5)); // calculates and returns 85
+console.log('2', memoized(5)); // returns 85 from the cache object
+console.log('2', memoized(5)); // returns 85 from the cache object
 
