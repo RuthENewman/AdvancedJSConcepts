833d63a384cce7dbf3096d711f00e88098abb44b
Ruth Newman <ruthnewman@gmail.com>
2021-01-13 23:49:06 +0000

    compose and pipe example

--- functional-programming/compose-and-pipe.js
+++ functional-programming/compose-and-pipe.js
@@ -5,5 +5,15 @@ const multiplyByThree = (num) => num * 3;
 const makePositive = (num) => Math.abs(num);
 const absoluteMltiplyByThree = compose(multiplyByThree, makePositive);
 
-const result = absoluteMltiplyByThree(-50);
-console.log(result);
+const composedResult = absoluteMltiplyByThree(-50);
+console.log(composedResult);
+
+// Pipe
+
+const pipe = (k, l) => (data) => l(k(data));
+const findAQuarter = (num) => num / 4;
+const roundUp = (num) => Math.ceil(num);
+const roundedQuarter = pipe(findAQuarter, roundUp);
+
+const pipedResult = roundedQuarter(30);
+console.log(pipedResult);
