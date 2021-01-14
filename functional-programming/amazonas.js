55085c3ae1143537dbfc844cc19147163ee33af0
Ruth Newman <ruthnewman@gmail.com>
2021-01-14 23:54:25 +0000

    refactored addItemToCart function

--- functional-programming/amazonas.js
+++ functional-programming/amazonas.js
@@ -5,28 +5,32 @@ const user = {
     cart: [],
     purchases: []
 }
+const compose = (f, g) => {...args} => f(g(...args));
 
-console.log(purchaseItem(user, {name: "Laptop", price: 650}));
+purchaseItem(
+    emptyCart,
+    buyItem,
+    addTax,
+    addItemToCart
+)(user, {name: "Laptop", price: 650});
 
-function purchaseItem(user, item) {
-    return Object.assign({}, user, {purchases: item});
-}
+const purchaseItem = (...fns) => fns.reduce(compose);
 
 const addItemToCart = (user, item) => {
-    user.cart = [...user.cart, item];
-    return user.cart;
+    const updatedCart = user.cart.concat(item);
+    return Object.assign({}, user, {cart: updatedCart });
 }
 
 const addTax = (user, item) => {
     const itemToTax = user.cart.find(itemInCart => itemInCart === item);
     itemToTax.price = (itemToTax.price * 1.03);
-    return user.cart;
+    return user;
 }
 
 const buyItem = (user, item) => {
     const itemInCart = user.cart.find(item => itemInCart === item);
     user.purchases = user.purchases.push(itemInCart);
-    return user.purchases;
+    return user;
 }
 
 const emptyCart = (user) => {
