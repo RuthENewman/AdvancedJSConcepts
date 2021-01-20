1cd276519b6c3ebc01843d65ef42aea4396ed1ab
Ruth Newman <ruthnewman@gmail.com>
2021-01-20 23:58:14 +0000

    fetch async await example

--- async-js/async-await.js
+++ async-js/async-await.js
@@ -1,11 +1,9 @@
-movePlayer(100, 'Left')
-    .then(() => movePlayer(400, 'Left'))
-    .then(() => movePlayer(10, 'Right'))
-    .then(() => movePlayer(330, 'Left'))
+// Fetch function example (fetch is a promise)
 
-async function playerStart() {
-    const first = await movePlayer(100, 'Left'); // pause
-    const second = await movePlayer(400, 'Left'); // pause
-    await movePlayer(10, 'Right'); // pause
-    await movePlayer(330, 'Left'); // pause
+async function fetchUsers() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json();
+    console.log(data);
 }
+
+fetchUsers();
