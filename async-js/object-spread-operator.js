ac1a94808ba885b5fed448c955881af9f727771c
Ruth Newman <ruthnewman@gmail.com>
2021-01-22 23:58:08 +0000

    refactored using objectSpread function

--- async-js/object-spread-operator.js
+++ async-js/object-spread-operator.js
@@ -1,18 +1,17 @@
 const animals = {
     lions: 8,
     tigers: 12,
-    bears: 24
+    bears: 24,
+    monkeys: 0
 }
 
-const { tigers, ...rest} = animals;
-
-console.log(tigers);
-console.log(rest);
-
-const array = [1,2,3,4,5,6,7,8,9,10];
-function sum(a, b, c, d, e, f, g, h, i, j) {
-    return a + b + c + d + e + f + g + h + i + j;
+function objectSpread(p1,p2,p3) {
+    console.log(p1);
+    console.log(p2);
+    console.log(p3);
 }
-console.log(sum(...array));
 
-const chords = ['Gm', 'Cm', 'Dm', 'Gm'];
+
+const { tigers, lions, ...rest} = animals;
+
+objectSpread(tigers, lions, rest);
