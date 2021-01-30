6945c61057a172894b21b4c19a32c3690f9608e9
Ruth Newman <ruthnewman@gmail.com>
2021-01-30 01:01:22 +0000

    commonJS and AMD syntax examples

---
+++ modules/amd.js
@@ -0,0 +1,15 @@
+// AMD syntax
+
+define(['module1', 'module2'],
+    function (module1Import, module2Import) {
+        var module1 = module1Import // .fight
+        var module2 = module2Import // .importedFunc2;
+
+    function dance() {
+
+    }
+s
+    return {
+        dance: dance,
+    };
+});
---
+++ modules/common.js
@@ -0,0 +1,12 @@
+// CommonJS syntax
+
+var module1 = require('module1'); // .fight
+var module2 = require('module2'); // .importedFunc2
+
+function fight() {
+
+}
+
+module.exports = {
+    fight: fight
+}
