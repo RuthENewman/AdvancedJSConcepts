bc77d25626f72ffcd8bdb10db09095913a6bbd83
Ruth Newman <ruthnewman@gmail.com>
2021-01-30 01:09:55 +0000

    default export

--- modules/es6.js
+++ modules/es6.js
@@ -5,7 +5,7 @@ function jump() {
 
 }
 
-export function fight(characterOne, characterTwo) {
+export default function fight(characterOne, characterTwo) {
     const attackOne = Math.floor(Math.random() * [characterOne.length]);
     const attackTwo = Math.floor(Math.random() * [characterTwo.length]);
     return attackOne > attackTwo ? `${characterOne} wins!` : `${characterTwo} wins!`;
