47c9aa802d4adda873a4ec50535ba53d95dd5c09
Ruth Newman <ruthnewman@gmail.com>
2021-02-01 23:38:24 +0000

    catch async error handling and logging

--- error-handling/async.js
+++ error-handling/async.js
@@ -1,11 +1,16 @@
 Promise.resolve('asyncfail')
     .then(response => {
-        throw new Error('# fail');
-        return response;
+        Promise.resolve().then(() => {
+            throw new Error('#3 fail');
+        }).catch(console.log)
+        return 5
     })
     .then(response => {
         console.log(response);
     })
+    .then(response => {
+        console.log(response.message);
+    })
     .catch(error => {
-        console.log(error);
+        console.log('final error');
     })
